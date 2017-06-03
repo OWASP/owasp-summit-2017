7e12b9d96fb8f58cbc6512c351671c3e0bd19464
Dinis Cruz <dinis.cruz@owasp.org>
2017-06-03 06:41:11 +0100

    Added redirect-to-https docker image code

--- src/docker/redirect-to-https/build.sh
+++ src/docker/redirect-to-https/build.sh
@@ -4,3 +4,5 @@ echo "Creating Docker image to redirect to https"
 
 docker build --no-cache=true  -t diniscruz/redirect-to-https .
 
+
+./start.sh
--- src/docker/redirect-to-https/start.sh
+++ src/docker/redirect-to-https/start.sh
@@ -5,9 +5,11 @@ echo "starting redirect to https"
 docker stop redirect-to-https
 docker rm redirect-to-https
 
-docker run  -p 80:80 --name=redirect-to-https diniscruz/redirect-to-https bash
+docker run  -d -p 80:80 --name=redirect-to-https diniscruz/redirect-to-https
 docker ps
 
 
 
+
+docker logs -f redirect-to-https
 
