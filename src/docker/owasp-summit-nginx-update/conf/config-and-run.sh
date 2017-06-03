e39752864213436dee25fb2927c0c383618051d8
Dinis Cruz <dinis.cruz@owasp.org>
2017-06-03 18:43:02 +0100

    Added ELK support to docker images

--- src/docker/owasp-summit-nginx-update/conf/config-and-run.sh
+++ src/docker/owasp-summit-nginx-update/conf/config-and-run.sh
@@ -3,6 +3,13 @@
 echo "Starting nginx"
 
 cd /etc/nginx/                                      # go to nginx folder
+
+if [ -z "$CERT_PWD" ]; then
+ echo "[ERROR]export CERT_PWD=d93882c2-bc29-4ffb-87d4-df895f6bf514: CERT_PWD enviroment variable is not set"
+ exit 1
+fi
 unzip -P ${CERT_PWD} ./encrypted-cert.zip           # unzip tls cert
+
+service filebeat restart
 
 nginx -g "daemon off;"
