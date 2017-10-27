196c62da0dbfa2943467554d33bb6706a344e6e2
Felipe Zipitría <fzipi@fing.edu.uy>
2017-10-26 23:42:00 -0300

    add docker build

--- src/docker/build.sh
+++ src/docker/build.sh
@@ -3,7 +3,7 @@
 echo "Creating Docker image for Owasp Summit site"
 
 docker build -t ruby-jekyll-node ./ruby-jekyll-node
-docker build --no-cache=true -t diniscruz/owasp-summit-base ./owasp-summit-base
+docker build --no-cache=true -t fzipi/owasp-summit-base ./owasp-summit-base
 
 
 
